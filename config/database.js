65ee31285fa907f98ae26034ca7fa368eb22288a
Markus Douglas Nascimento Siqueira <makusdouglas@gmail.com>
2020-11-09 02:13:23 -0300

    initial commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-blog-cms'),
+        username: env('DATABASE_USERNAME', 'dsstore'),
+        password: env('DATABASE_PASSWORD', 'da272abfd6e1ddb7d506f78fd79f785c'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e61e1948-6e78-4ee6-8ac4-c7afcd643112'
+};
